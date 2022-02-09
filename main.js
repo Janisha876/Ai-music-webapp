b33a11318f8a046ee611f171d7534a169565c1db
Janisha876 <94450884+Janisha876@users.noreply.github.com>
2022-02-09 16:32:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song="";
 song1="";
+leftwristx=0;
+rightwristx=0;
+leftwristy=0;
+rightwristy=0;
 function preload(){
     song=loadSound("music.mp3");
     song1=loadSound("music2.mp3");
@@ -10,7 +14,21 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+ if(results.length>0){
+     console.log(results);
+     leftwristx=results[0].pose.leftWrist.x;
+     rightwristx=results[0].pose.rightWrist.x;
+     leftwristy=results[0].pose.leftWrist.y;
+     rightwristy=results[0].pose.rightWrist.y;
+ }
 }
